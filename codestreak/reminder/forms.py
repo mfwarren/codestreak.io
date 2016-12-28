f3a5dae83f925f262fdac2644430c6a15bc3c5e4
Matt Warren <matt.warren@gmail.com>
2016-12-28 13:28:57 -0500

    lookup phone numbers to check if they are valid

--- codestreak/reminder/forms.py
+++ codestreak/reminder/forms.py
@@ -2,6 +2,9 @@
 """User forms."""
 from flask_wtf import Form
 from wtforms import BooleanField, StringField
+from wtforms.validators import ValidationError
+
+from twilio.rest.lookups import TwilioLookupsClient
 
 
 class EditReminder(Form):
@@ -15,3 +18,12 @@ class EditReminder(Form):
     def __init__(self, *args, **kwargs):
         """Create instance."""
         super(EditReminder, self).__init__(*args, **kwargs)
+
+    def validate_sms_number(self, field):
+        try:
+            client = TwilioLookupsClient()
+            match = client.phone_numbers.get(field.data)
+            field.data = match.phone_number
+        except Exception as ex:
+            print(ex)
+            raise ValidationError('Failed to validate this phone number')
