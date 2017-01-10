9c85be00b9bd7a39899c6d5402d5c97796d5a277
Matt Warren <matt.warren@gmail.com>
2017-01-09 21:18:44 -0500

    adding some new messages for SMSes

--- codestreak/sms.py
+++ codestreak/sms.py
@@ -5,7 +5,22 @@ import random
 from twilio.rest import TwilioRestClient
 from twilio.rest.exceptions import TwilioRestException
 
-from .email import SUBJECT_LINES
+SUBJECT_LINES = [
+    'CodeStreak.io: reminder to commit!',
+    'Even source code needs love sometimes',
+    '⏰ time to code ⏰',
+    '⭐⭐⭐ gold stars if you can push some code',
+    '🌠 Keep the streak going!',
+    'do some coding then celebrate! 🍺🍺🍻 🎉🎉🎉',
+    'keep your perfect score going 💯',
+    'get coding! (reminder you can reply with STOP to cancel these messages)',
+    "I'm still waiting to see some code from you today",
+    "GitHub misses you",
+    'Just write something quick',
+    '15 minutes of practice coding could level you up',
+    "here's a challenge: how many ways do you know to print to stdout",
+    "I'm watching your account, waiting to see your next brilliant commit"
+]
 
 
 def sms_notify(github_event, reminder):
